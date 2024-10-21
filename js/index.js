5e990e545910c4bb87b26077f1a55d496c2f6323
raji-ayyub <ayyubraji87@gmail.com>
2024-10-22 00:10:12 +0100

    Feat: Implement statistics for stat-cards events, speakers, registrations, revenue

--- js/index.js
+++ js/index.js
@@ -3,44 +3,44 @@
 
 
 // DEMO RANDOM DATA ---------------------------------------
-
 const eventsData = [
-    { eventName: "Cloud Innovation Summit", date: "2024-01-05", speaker: "Sundar Pichai", status: "Completed", description: "Exploring the future of cloud technologies.", attendees: 300 },
-    { eventName: "Tech Leaders Conference", date: "2024-01-10", speaker: "Satya Nadella", status: "completed", description: "Leadership insights in the tech industry.", attendees: 450 },
-    { eventName: "AI & Future Technologies", date: "2024-01-15", speaker: "Jane Doe", status: "inprogress", description: "Advancements in AI and emerging technologies.", attendees: 200 },
-    { eventName: "Web Development Workshop", date: "2024-01-25", speaker: "Mark Zuckerberg", status: "completed", description: "Hands-on learning for web developers.", attendees: 250 },
-    { eventName: "Cybersecurity Roundtable", date: "2024-02-01", speaker: "Sheryl Sandberg", status: "Completed", description: "Roundtable on the latest cybersecurity trends.", attendees: 350 },
-    { eventName: "Data Science Symposium", date: "2024-02-05", speaker: "Elon Musk", status: "completed", description: "Deep dive into data science innovations.", attendees: 500 },
-    { eventName: "Blockchain Bootcamp", date: "2024-02-10", speaker: "Vitalik Buterin", status: "Completed", description: "Understanding blockchain technology.", attendees: 300 },
-    { eventName: "UX/UI Design Forum", date: "2024-02-15", speaker: "Marissa Mayer", status: "inprogress", description: "The latest in UX/UI design strategies.", attendees: 220 },
-    { eventName: "Startup Pitch Day", date: "2024-03-20", speaker: "Peter Thiel", status: "Completed", description: "Startups pitching their innovative ideas.", attendees: 400 },
-    { eventName: "Cloud Security Summit", date: "2024-04-01", speaker: "Diane Greene", status: "inprogress", description: "Ensuring security in cloud environments.", attendees: 250 },
-    { eventName: "Internet of Things Expo", date: "2024-05-05", speaker: "Tim Cook", status: "Completed", description: "Showcasing IoT advancements.", attendees: 350 },
-    { eventName: "Mobile Development Workshop", date: "2024-05-10", speaker: "Sundar Pichai", status: "inprogress", description: "Workshop on mobile app development.", attendees: 230 },
-    { eventName: "Agile Methodologies Conference", date: "2024-05-15", speaker: "Jack Dorsey", status: "Completed", description: "Best practices in agile development.", attendees: 275 },
-    { eventName: "Digital Marketing Strategies", date: "2024-05-20", speaker: "Susan Wojcicki", status: "inprogress", description: "Innovative digital marketing strategies.", attendees: 220 },
-    { eventName: "Artificial Intelligence Summit", date: "2024-05-25", speaker: "Andrew Ng", status: "inprogress", description: "Exploring AI advancements and trends.", attendees: 400 },
-    { eventName: "Fintech Innovations Forum", date: "2024-05-28", speaker: "Christine Lagarde", status: "inprogress", description: "Innovations shaping the future of fintech.", attendees: 350 },
-    { eventName: "Remote Work Conference", date: "2024-06-05", speaker: "Reed Hastings", status: "inprogress", description: "The evolution of remote work models.", attendees: 260 },
-    { eventName: "Tech for Good Summit", date: "2024-06-10", speaker: "Jeff Bezos", status: "inprogress", description: "Technology driving social good.", attendees: 290 },
-    { eventName: "Ethical Hacking Workshop", date: "2024-07-01", speaker: "Kevin Mitnick", status: "Completed", description: "Hands-on ethical hacking training.", attendees: 210 },
-    { eventName: "Open Source Forum", date: "2024-08-02", speaker: "Linus Torvalds", status: "completed", description: "Discussions on open-source software.", attendees: 380 },
-    { eventName: "Machine Learning Symposium", date: "2024-08-05", speaker: "Demis Hassabis", status: "Completed", description: "Symposium on machine learning advancements.", attendees: 410 },
-    { eventName: "Software Engineering Conference", date: "2024-08-10", speaker: "Larry Page", status: "inprogress", description: "Engineering solutions for modern software.", attendees: 330 },
-    { eventName: "Product Management Workshop", date: "2024-08-15", speaker: "Eric Schmidt", status: "Completed", description: "Best practices in product management.", attendees: 245 },
-    { eventName: "DevOps Summit", date: "2024-09-05", speaker: "Werner Vogels", status: "inprogress", description: "Latest trends in DevOps.", attendees: 400 },
-    { eventName: "Virtual Reality Expo", date: "2024-09-10", speaker: "Palmer Luckey", status: "Completed", description: "Showcase of virtual reality technologies.", attendees: 500 },
-    { eventName: "Tech Innovations Conference", date: "2024-09-20", speaker: "Tony Fadell", status: "inprogress", description: "Exploring cutting-edge tech innovations.", attendees: 300 },
-    { eventName: "Cloud Migration Workshop", date: "2024-10-01", speaker: "Ava Perez", status: "inprogress", description: "Best practices in cloud migration.", attendees: 240 },
-    { eventName: "Data Visualization Forum", date: "2024-10-10", speaker: "Alexis Ohanian", status: "inprogress", description: "Exploring new trends in data visualization.", attendees: 310 },
-    { eventName: "Internet Security Conference", date: "2024-10-15", speaker: "Charlotte Johnson", status: "Completed", description: "Protecting internet infrastructure.", attendees: 320 },
-    { eventName: "Tech Diversity Summit", date: "2024-11-01", speaker: "Melinda Gates", status: "inprogress", description: "Promoting diversity in the tech sector.", attendees: 350 },
-    { eventName: "Sustainable Tech Forum", date: "2024-12-01", speaker: "Daniel Robinson", status: "Completed", description: "Sustainable technology innovations.", attendees: 230 },
-    { eventName: "E-Commerce Innovations Workshop", date: "2024-12-10", speaker: "Emily Martinez", status: "inprogress", description: "Exploring the future of e-commerce.", attendees: 290 },
-    { eventName: "Smart Cities Conference", date: "2024-12-15", speaker: "Oliver Davis", status: "Completed", description: "Innovations in smart city development.", attendees: 400 },
-    { eventName: "Digital Transformation Summit", date: "2024-12-20", speaker: "Mia Thomas", status: "inprogress", description: "Driving digital transformation strategies.", attendees: 270 },
-    { eventName: "Tech Education Forum", date: "2024-12-25", speaker: "William Clark", status: "Completed", description: "The future of tech education.", attendees: 320 }
+    { eventName: "Cloud Innovation Summit", date: "2024-01-05", speaker: "Sundar Pichai", status: "Completed", description: "Exploring the future of cloud technologies.", attendees: 300, ticketPrice: 150, sponsorship: 5000 },
+    { eventName: "Tech Leaders Conference", date: "2024-01-10", speaker: "Satya Nadella", status: "completed", description: "Leadership insights in the tech industry.", attendees: 450, ticketPrice: 200, sponsorship: 7000 },
+    { eventName: "AI & Future Technologies", date: "2024-01-15", speaker: "Jane Doe", status: "inprogress", description: "Advancements in AI and emerging technologies.", attendees: 200, ticketPrice: 120, sponsorship: 4000 },
+    { eventName: "Web Development Workshop", date: "2024-01-25", speaker: "Mark Zuckerberg", status: "completed", description: "Hands-on learning for web developers.", attendees: 250, ticketPrice: 100, sponsorship: 3000 },
+    { eventName: "Cybersecurity Roundtable", date: "2024-02-01", speaker: "Sheryl Sandberg", status: "Completed", description: "Roundtable on the latest cybersecurity trends.", attendees: 350, ticketPrice: 180, sponsorship: 6000 },
+    { eventName: "Data Science Symposium", date: "2024-02-05", speaker: "Elon Musk", status: "completed", description: "Deep dive into data science innovations.", attendees: 500, ticketPrice: 250, sponsorship: 8000 },
+    { eventName: "Blockchain Bootcamp", date: "2024-02-10", speaker: "Vitalik Buterin", status: "Completed", description: "Understanding blockchain technology.", attendees: 300, ticketPrice: 170, sponsorship: 4500 },
+    { eventName: "UX/UI Design Forum", date: "2024-02-15", speaker: "Marissa Mayer", status: "inprogress", description: "The latest in UX/UI design strategies.", attendees: 220, ticketPrice: 140, sponsorship: 3500 },
+    { eventName: "Startup Pitch Day", date: "2024-03-20", speaker: "Peter Thiel", status: "Completed", description: "Startups pitching their innovative ideas.", attendees: 400, ticketPrice: 220, sponsorship: 5000 },
+    { eventName: "Cloud Security Summit", date: "2024-04-01", speaker: "Diane Greene", status: "inprogress", description: "Ensuring security in cloud environments.", attendees: 250, ticketPrice: 160, sponsorship: 4000 },
+    { eventName: "Internet of Things Expo", date: "2024-05-05", speaker: "Tim Cook", status: "Completed", description: "Showcasing IoT advancements.", attendees: 350, ticketPrice: 190, sponsorship: 6500 },
+    { eventName: "Mobile Development Workshop", date: "2024-05-10", speaker: "Sundar Pichai", status: "inprogress", description: "Workshop on mobile app development.", attendees: 230, ticketPrice: 130, sponsorship: 3000 },
+    { eventName: "Agile Methodologies Conference", date: "2024-05-15", speaker: "Jack Dorsey", status: "Completed", description: "Best practices in agile development.", attendees: 275, ticketPrice: 170, sponsorship: 4500 },
+    { eventName: "Digital Marketing Strategies", date: "2024-05-20", speaker: "Susan Wojcicki", status: "inprogress", description: "Innovative digital marketing strategies.", attendees: 220, ticketPrice: 150, sponsorship: 3000 },
+    { eventName: "Artificial Intelligence Summit", date: "2024-05-25", speaker: "Andrew Ng", status: "inprogress", description: "Exploring AI advancements and trends.", attendees: 400, ticketPrice: 200, sponsorship: 7000 },
+    { eventName: "Fintech Innovations Forum", date: "2024-05-28", speaker: "Christine Lagarde", status: "inprogress", description: "Innovations shaping the future of fintech.", attendees: 350, ticketPrice: 210, sponsorship: 6000 },
+    { eventName: "Remote Work Conference", date: "2024-06-05", speaker: "Reed Hastings", status: "inprogress", description: "The evolution of remote work models.", attendees: 260, ticketPrice: 180, sponsorship: 5000 },
+    { eventName: "Tech for Good Summit", date: "2024-06-10", speaker: "Jeff Bezos", status: "inprogress", description: "Technology driving social good.", attendees: 290, ticketPrice: 170, sponsorship: 4500 },
+    { eventName: "Ethical Hacking Workshop", date: "2024-07-01", speaker: "Kevin Mitnick", status: "Completed", description: "Hands-on ethical hacking training.", attendees: 210, ticketPrice: 160, sponsorship: 3000 },
+    { eventName: "Open Source Forum", date: "2024-08-02", speaker: "Linus Torvalds", status: "completed", description: "Discussions on open-source software.", attendees: 380, ticketPrice: 140, sponsorship: 4000 },
+    { eventName: "Machine Learning Symposium", date: "2024-08-05", speaker: "Demis Hassabis", status: "Completed", description: "Symposium on machine learning advancements.", attendees: 410, ticketPrice: 200, sponsorship: 7000 },
+    { eventName: "Software Engineering Conference", date: "2024-08-10", speaker: "Larry Page", status: "inprogress", description: "Engineering solutions for modern software.", attendees: 330, ticketPrice: 190, sponsorship: 5500 },
+    { eventName: "Product Management Workshop", date: "2024-08-15", speaker: "Eric Schmidt", status: "Completed", description: "Best practices in product management.", attendees: 245, ticketPrice: 180, sponsorship: 3500 },
+    { eventName: "DevOps Summit", date: "2024-09-05", speaker: "Werner Vogels", status: "inprogress", description: "Latest trends in DevOps.", attendees: 400, ticketPrice: 190, sponsorship: 6000 },
+    { eventName: "Virtual Reality Expo", date: "2024-09-10", speaker: "Palmer Luckey", status: "Completed", description: "Showcase of virtual reality technologies.", attendees: 500, ticketPrice: 220, sponsorship: 7500 },
+    { eventName: "Tech Innovations Conference", date: "2024-09-20", speaker: "Tony Fadell", status: "inprogress", description: "Exploring cutting-edge tech innovations.", attendees: 300, ticketPrice: 180, sponsorship: 5000 },
+    { eventName: "Cloud Migration Workshop", date: "2024-10-01", speaker: "Ava Perez", status: "inprogress", description: "Best practices in cloud migration.", attendees: 240, ticketPrice: 160, sponsorship: 3500 },
+    { eventName: "Data Visualization Forum", date: "2024-10-10", speaker: "Alexis Ohanian", status: "inprogress", description: "Exploring new trends in data visualization.", attendees: 310, ticketPrice: 170, sponsorship: 4000 },
+    { eventName: "Internet Security Conference", date: "2024-10-15", speaker: "Charlotte Johnson", status: "Completed", description: "Protecting internet infrastructure.", attendees: 320, ticketPrice: 200, sponsorship: 6000 },
+    { eventName: "Tech Diversity Summit", date: "2024-11-01", speaker: "Melinda Gates", status: "inprogress", description: "Promoting diversity in the tech sector.", attendees: 350, ticketPrice: 190, sponsorship: 4500 },
+    { eventName: "Sustainable Tech Forum", date: "2024-12-01", speaker: "Daniel Robinson", status: "Completed", description: "Sustainable technology innovations.", attendees: 230, ticketPrice: 150, sponsorship: 3000 },
+    { eventName: "E-Commerce Innovations Workshop", date: "2024-12-10", speaker: "Emily Martinez", status: "inprogress", description: "Exploring the future of e-commerce.", attendees: 290, ticketPrice: 170, sponsorship: 4000 },
+    { eventName: "Smart Cities Conference", date: "2024-12-15", speaker: "Oliver Davis", status: "Completed", description: "Innovations in smart city development.", attendees: 400, ticketPrice: 200, sponsorship: 5000 },
+    { eventName: "Digital Transformation Summit", date: "2024-12-20", speaker: "Mia Thomas", status: "inprogress", description: "Driving digital transformation strategies.", attendees: 270, ticketPrice: 180, sponsorship: 3500 },
+    { eventName: "Tech Education Forum", date: "2024-12-25", speaker: "William Clark", status: "Completed", description: "The future of tech education.", attendees: 320, ticketPrice: 160, sponsorship: 3000 }
 ];
+
 
 
 
@@ -842,5 +842,67 @@ document.addEventListener("DOMContentLoaded", function() {
     
 
     modalCard();
+
+
+
+
+
+
+
+
+
+    // ----------STATISTICS ---------------------
+
+// const statisticsCardList = document.querySelectorAll(".stat-div");
+
+    const statCardTotalEvents = document.querySelector(".stat-data-total");
+    const totalEvents = Math.ceil(eventsData.length);
+
+    statCardTotalEvents.textContent = totalEvents;
+
+
+  
+    const statActiveSpeakers = document.querySelector(".active-speakers");
+
+    const uniqueSpeakers = new Set(eventsData.map(event => event.speaker));
+    
+    const totalSpeakers = uniqueSpeakers.size + 1;
+    
+    statActiveSpeakers.textContent = totalSpeakers;
+    
+    
+
+
+
+
+
+    const totalAttendees = eventsData.reduce((sum, event) => sum + (event.attendees || 0), 0);
+
+    const statCardAttendees = document.querySelector(".total-regi")
+    console.log(totalAttendees);
+    statCardAttendees.textContent = totalAttendees;
+
+
+    
+
+    const statCardRevenue = document.querySelector(".total-revenue")
+
+    const calculateTotalRevenue = (events) => {
+        return events.reduce((totalRevenue, event) => {
+            const eventRevenue = (event.ticketPrice * event.attendees) + event.sponsorship;
+            return totalRevenue + eventRevenue;
+        }, 0);
+    };
+    
+    const totalRevenue = calculateTotalRevenue(eventsData);
+    statCardRevenue.textContent = totalRevenue;
+    console.log("Total Revenue:", totalRevenue);
+    
+    
+   
+    
+    
+
+
 
 
